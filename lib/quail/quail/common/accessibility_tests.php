1744beb9f5c77cd1e5dfa31da8ec66ab4e25854e
Jose Silveti <jose.silveti@ucf.edu>
2019-07-18 15:43:43 -0400

    testing out videos mbedded error

--- lib/quail/quail/common/accessibility_tests.php
+++ lib/quail/quail/common/accessibility_tests.php
@@ -6434,31 +6434,7 @@ class videosEmbeddedOrLinkedNeedCaptions extends quailTest
 	*/
 	function check()
 	{
-		$search_youtube = '/(youtube|youtu.be)/';
-		$search_vimeo = '/(vimeo)/';
 
-		foreach ($this->getAllElements(array('a', 'embed', 'iframe')) as $video) {
-			$attr = ($video->tagName == 'a') ? 'href' : 'src';
-			if ($video->hasAttribute($attr)) {
-				$attr_val = $video->getAttribute($attr);
-				if ( preg_match($search_youtube, $attr_val) ) {
-					$service = 'youtube';
-				}
-				elseif ( preg_match($search_vimeo, $attr_val) ) {
-					$service = 'vimeo';
-				}
-				if (isset($service)) {
-					if ($service->videoUnavailable($attr_val)) {
-							$this->addReport($video);
-						
-						$captionState = $this->services[$service]->captionsMissing($attr_val);
-						if($captionState != 2) {
-						$this->addReport($video, null, null, $captionState, ($captionState == 1));
-						}
-					}
-				}
-			}
-		}
 	}
 }
 
